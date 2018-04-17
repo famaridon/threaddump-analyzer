5fcaf9d33d14ccdb2f89dc36e8b11bbcd92e6022
famaridon <famaridon@visiativ.com>
2018-04-17 23:22:25 +0200

    rework upload

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,6 +3,7 @@ import {MatDialog} from '@angular/material';
 import {UploadDialogComponent} from '../upload-dialog/upload-dialog.component';
 import {StoreService} from '../../services/store.service';
 import {Threaddump} from '../../services/threaddump';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-header',
@@ -11,21 +12,28 @@ import {Threaddump} from '../../services/threaddump';
 })
 export class HeaderComponent implements OnInit {
 
-  private _storage: Map<string, Promise<Threaddump>>;
+  private _storage: Observable<Promise<Threaddump>[]>;
+  private _threaddumps: Promise<Threaddump>[];
 
   constructor(public storeService: StoreService, public dialog: MatDialog) {
     this._storage = this.storeService.storage;
+    this._storage.subscribe((threaddumps) => {
+      this._threaddumps = threaddumps;
+    });
   }
 
   ngOnInit() {
   }
 
+  public clearStore(): void {
+    this.storeService.clear();
+  };
+
   public openDialog(): void {
-    this.dialog.open(UploadDialogComponent, { });
+    this.dialog.open(UploadDialogComponent, {});
   }
 
-
-  get storage(): Map<string, Promise<Threaddump>> {
-    return this._storage;
+  get threaddumps(): Promise<Threaddump>[] {
+    return this._threaddumps;
   }
 }
--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -1,26 +1,28 @@
 import { Injectable } from '@angular/core';
 import {Threaddump} from './threaddump';
-import { UUID } from 'angular2-uuid';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import {Observable} from 'rxjs/Observable';
 
 
 @Injectable()
 export class StoreService {
 
-  private _storage: Map<string, Promise<Threaddump>> = new Map<string, Promise<Threaddump>>();
+  private _storage: Promise<Threaddump>[] = [] ;
+  private subject: BehaviorSubject<Promise<Threaddump>[]> = new BehaviorSubject([]);
 
   constructor() { }
 
-  public save(threaddump: Promise<Threaddump>): string {
-    const id = UUID.UUID();
-    this._storage.set(id, threaddump);
-    return id;
+  public save(threaddump: Promise<Threaddump>): void {
+    this._storage.push( threaddump);
+    this.subject.next(this._storage.slice(0));
   }
 
-  public find(id: string): Promise<Threaddump> {
-    return this._storage.get(id);
+  public clear(): void {
+    this._storage = [];
+    this.subject.next(this._storage);
   }
 
-  get storage(): Map<string, Promise<Threaddump>> {
-    return this._storage;
+  get storage(): Observable<Promise<Threaddump>[]> {
+    return this.subject.asObservable();
   }
 }
